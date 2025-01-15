60e4a6c234992a9635c29f6bb70cf7f810311fc6
cedrulion <byiringirocedric012@gmail.com>
2025-01-15 14:09:26 +0200

    remove field

--- server.js
+++ server.js
@@ -9,16 +9,30 @@ const transactionRoutes = require('./routes/transactionRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
 const errorMiddleware = require('./middleware/errorMiddleware');
 
+// Set up database connection
 connectDB();
-const app = express();
-app.use(express.json());
-app.use(cors());
 
+const app = express();
+
+// CORS configuration to allow frontend URL
+const corsOptions = {
+  origin: process.env.NODE_ENV === 'production' ? 'https://walletapp-9wli.onrender.com' : 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true  // Allow credentials (cookies, etc.)
+};
+
+// Apply CORS middleware with the configuration
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+// Route handlers
 app.use('/api/accounts', accountRoutes);
 app.use('/api/transactions', transactionRoutes);
 app.use('/api/categories', categoryRoutes);
 
+// Error handling middleware
 app.use(errorMiddleware);
 
-
+// Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
